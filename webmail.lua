3fde9b7538bdce9ff8ded61d78c3818c38707f29
Alex Kinsella <alex.kinsella@gmail.com>
2014-10-08 16:53:27 -0400

    Update webmail.lua
    Added Spread Fingers and wave down to reply all and Spread Fingers and wave up to return to conversation list.

--- webmail.lua
+++ webmail.lua
@@ -6,10 +6,16 @@ scriptId = 'com.thalmic.scripts.gmail'
 -- First, go to Settings and enable "Keyboard Shortcuts"
 -- Second, go to Settings > Labs and enable the "Auto-advance" tool
 
+-- Wave Left to Archive
+-- Wave Right to Reply
+-- Fist to go to next message
+-- Spread Fingers and release up to go back to conversation
+-- Spread Fingers and release down to reply all
+
 -- Effects
 
 function composeEmail()
-    myo.keyboard("c", "press", "shift")
+    myo.keyboard("c", "press")
 end
 
 function archiveConversation()
@@ -30,6 +36,10 @@ end
 
 function nextOlderMessage()
     myo.keyboard("j", "press")
+end
+
+function conversationScreen()
+    myo.keyboard("u", "press")
 end
 
 -- Helpers
@@ -61,6 +71,11 @@ end
 
 function onPoseEdge(pose, edge)
     if pose == "thumbToPinky" then
+        -- Pitch... 0.30750495195389
+        -- Yaw... 2.7852053642273
+        --myo.debug("Pitch... "..currentPitch)
+        --myo.debug("Yaw... "..currentYaw)
+        -- myo.debug("Roll... "..currentRoll)
         if edge == "off" then
             enabled = true
             enabledSince = myo.getTimeMilliseconds()
@@ -71,28 +86,41 @@ function onPoseEdge(pose, edge)
         end
     end
 
-    if enabled and edge == "on" then
+    if enabled then
+    --if enabled and edge == "on" then
         pose = conditionallySwapWave(pose)
 
-        if pose == "waveOut" then
+        if pose == "waveOut" and edge == "on" then
             myo.vibrate("short")
             enabled = false
             replyToOne()
         end
-        if pose == "waveIn" then
+        if pose == "waveIn" and edge == "on" then
+            if currentYaw > 1.5 then
+               -- myo.debug('great than 1.5 '..currentYaw)
+            else
+                --myo.debug('less than 1.5 '..currentYaw)
+            end
             myo.vibrate("short")
             enabled = false
             archiveConversation()
         end
-        if pose == "fist" then
+        if pose == "fist" and edge == "on" then
             myo.vibrate("short")
             enabled = false
             nextOlderMessage()
         end
         if pose == "fingersSpread" then
-            myo.vibrate("medium")
-            enabled = true
-            replyToAll()
+           -- myo.debug("hey.. "..currentYaw)
+            if edge == "off" and currentPitch < 0.1 then
+                myo.vibrate("medium")
+                enabled = true
+                replyToAll()
+            elseif edge == "off" and currentPitch > 0.2 then
+                myo.vibrate("short")
+                enabled = true
+                conversationScreen()
+            end
         end
     end
 end
@@ -101,9 +129,14 @@ end
 ENABLED_TIMEOUT = 2200
 currentYaw = 0
 currentPitch = 0
+currentRoll = 0
 
 function onPeriodic()
-    if enabled then
+    currentYaw = myo.getYaw()
+    currentPitch = myo.getPitch()
+    currentRoll = myo.getRoll()
+    
+   if enabled then
 
         if myo.getTimeMilliseconds() - enabledSince > ENABLED_TIMEOUT then
             enabled = false
